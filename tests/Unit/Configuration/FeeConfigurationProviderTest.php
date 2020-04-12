70071e08b39eb888853b58d06dd2f92ca37c5c59
Doncho Toromanov <doncho.t@gmail.com>
2020-04-12 18:16:14 +0300

    Unit tests

--- tests/Unit/Configuration/FeeConfigurationProviderTest.php
+++ tests/Unit/Configuration/FeeConfigurationProviderTest.php
@@ -7,34 +7,45 @@ use PHPUnit\Framework\TestCase;
 
 class FeeConfigurationProviderTest extends TestCase
 {
+    /**
+     * @var \App\Configuration\FeeConfigurationProvider
+     */
+    private $provider;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->provider = new FeeConfigurationProvider('20', '30.7', ['250', 'GBP'], ['500', 'USD'], ['1000.56', 'EUR'], 7);
+    }
 
     public function testGetWeeklyCashOutAmountLimit()
     {
-
+        $this->assertEquals(['1000.56', 'EUR'], $this->provider->getWeeklyCashOutAmountLimit());
     }
 
     public function testGetCashInFeePercentage()
     {
-
+        $this->assertEquals('30.7', $this->provider->getCashInFeePercentage());
     }
 
     public function testGetMaxCashInFee()
     {
-
+        $this->assertEquals(['250', 'GBP'], $this->provider->getMaxCashInFee());
     }
 
     public function testGetWeeklyCashOutCountLimit()
     {
-
+        $this->assertEquals(7, $this->provider->getWeeklyCashOutCountLimit());
     }
 
     public function testGetCashOutFeePercentage()
     {
-
+        $this->assertEquals('20', $this->provider->getCashOutFeePercentage());
     }
 
     public function testGetMinCashOutFee()
     {
-
+        $this->assertEquals(['500', 'USD'], $this->provider->getMinCashOutFee());
     }
 }
--- tests/Unit/Configuration/MoneyConfigurationProviderTest.php
+++ tests/Unit/Configuration/MoneyConfigurationProviderTest.php
@@ -7,19 +7,78 @@ use PHPUnit\Framework\TestCase;
 
 class MoneyConfigurationProviderTest extends TestCase
 {
+    /**
+     * @var \App\Configuration\MoneyConfigurationProvider
+     */
+    private $provider;
 
-    public function testGetDefaultPrecision()
+    /**
+     * @var array
+     */
+    private $supportedCurrencies;
+
+    protected function setUp()
     {
+        parent::setUp();
+        $currencyPrecisions = ['EUR' => 2, 'USD' => 2, 'JPY' => 0];
+        $this->supportedCurrencies = ['EUR', 'USD', 'JPY'];
+        $conversionRates = ['EUR:USD' => '1.1497', 'EUR:JPY' => '129.53', 'AB:CD' => '1234'];
 
+        $this->provider = new MoneyConfigurationProvider($currencyPrecisions, $this->supportedCurrencies, $conversionRates);
+    }
+
+    /**
+     * @dataProvider defaultPrecisionProvider
+     * @param string $currency
+     * @param int $expected
+     */
+    public function testGetDefaultPrecision($currency, $expected)
+    {
+        $this->assertEquals($expected, $this->provider->getDefaultPrecision($currency));
+    }
+
+    public function testGetDefaultPrecisionWithInvalidData()
+    {
+        $default = 5;
+        $this->assertEquals($default, $this->provider->getDefaultPrecision('TEST', $default));
     }
 
     public function testGetSupportedCurrencies()
     {
-
+        $this->assertEquals($this->supportedCurrencies, $this->provider->getSupportedCurrencies());
     }
 
-    public function testGetConversionRate()
+    /**
+     * @dataProvider conversionRatesProvider
+     * @param string $currencyFrom
+     * @param string $currencyTo
+     * @param string $expected
+     */
+    public function testGetConversionRate($currencyFrom, $currencyTo, $expected)
     {
+        $this->assertEquals($expected, $this->provider->getConversionRate($currencyFrom, $currencyTo));
+    }
 
+    public function testGetConversionRateWithInvalidData()
+    {
+        $this->assertEquals(false, $this->provider->getConversionRate('money1', 'money2'));
+    }
+
+    public function defaultPrecisionProvider()
+    {
+        return [
+            ['EUR', 2],
+            ['USD', 2],
+            ['JPY', 0],
+        ];
+    }
+
+    public function conversionRatesProvider()
+    {
+        return [
+            ['EUR', 'USD', '1.1497'],
+            ['EUR', 'JPY', '129.53'],
+            ['AB', 'CD', '1234'],
+        ];
     }
 }
