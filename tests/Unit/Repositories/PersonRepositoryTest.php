40a74f3ffac8570678b41eee836d4098a42518e2
Doncho Toromanov <doncho.t@gmail.com>
2020-04-12 17:35:20 +0300

    Unit tests

--- tests/Unit/Repositories/PersonRepositoryTest.php
+++ tests/Unit/Repositories/PersonRepositoryTest.php
@@ -2,24 +2,49 @@
 
 namespace Tests\Unit\Repositories;
 
+use App\MoneyCalculator;
+use App\Person;
 use App\Repositories\PersonRepository;
 use PHPUnit\Framework\TestCase;
 
 class PersonRepositoryTest extends TestCase
 {
+    /**
+     * @var \App\Repositories\PersonRepository
+     */
+    private $repository;
 
-    public function testAddPerson()
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $calculator;
+
+    protected function setUp()
     {
+        parent::setUp();
 
+        $this->repository = new PersonRepository();
+        $this->calculator = $this->createMock(MoneyCalculator::class);
     }
 
-    public function testGetPerson()
+    public function testAddGetPerson()
     {
+        $person = new Person(4, $this->calculator);
+        $this->repository->addPerson($person);
+        $resultPerson = $this->repository->getPerson(4);
 
+        $this->assertEquals($person, $resultPerson);
     }
 
     public function testDeleteAll()
     {
+        $person = new Person(4, $this->calculator);
+        $this->repository->addPerson($person);
 
+        $this->repository->deleteAll();
+
+        $resultPerson = $this->repository->getPerson(4);
+
+        $this->assertNotEquals($person, $resultPerson);
     }
 }
